6985e29e4e7e805077e1af18de27ea81dfeb8738
Filipo <filipfilip280@gmail.com>
2024-07-26 00:49:17 +0200

    chore: remove unnecessary wrapper component

--- app/create-status-modal.tsx
+++ app/create-status-modal.tsx
@@ -1,10 +1,5 @@
 import { CreateStatus } from "@/features/create-status/public-api";
-import { View } from "react-native-ui-lib";
 
 export default function CreateStatusModalScreen() {
-  return (
-    <View>
-      <CreateStatus />
-    </View>
-  );
+  return <CreateStatus />;
 }
